17f251bc20033e81b7e77fd0065708eab603a290
S0rrow <jae9hell@gmail.com>
2024-05-02 17:29:14 +0900

    settings.py updated

--- pybo/config/settings.py
+++ pybo/config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = keys['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1",]
+ALLOWED_HOSTS = ["127.0.0.1", "192.168.45.250", "server.local"]
 
 SECURE_REFERRER_POLICY = "no-referrer-when-downgrade"
 # Application definition
