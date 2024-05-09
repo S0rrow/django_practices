a45918d0bc74dc93a5b21d5a3e22e0e8893024d5
S0rrow <jae9hell@gmail.com>
2024-05-09 09:02:08 +0900

    answering system added for django server

--- pybo/config/settings.py
+++ pybo/config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = keys['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "192.168.45.250", "server.local"]
+ALLOWED_HOSTS = ["server.local"]
 
 SECURE_REFERRER_POLICY = "no-referrer-when-downgrade"
 # Application definition
@@ -121,7 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- pybo/main/views.py
+++ pybo/main/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from .models import Question
+from django.utils import timezone
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello")
+    question_list = Question.objects.order_by('-create_date')
+    return render(
+        request=request,
+        template_name='main/question_list.html',
+        context={'question_list' : question_list}
+    )
     
+def detail(request, question_id):
+    q = get_object_or_404(Question, pk=question_id)
+    return render(request, 'main/question_detail.html', {'question':q})
+
+def answer_create(request, question_id):
+    q = get_object_or_404(Question, pk=question_id)
+    q.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    return redirect('main:detail', question_id=q.id)
